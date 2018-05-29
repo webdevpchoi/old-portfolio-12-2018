062cb5c46ff4a389e4c9a7a66f2ed0662991281d
Phillip Choi <pchoi13370@gmail.com>
2018-05-28 22:42:16 -0700

    Added nodemailer, set up environmental variables, and tested gmail functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const 	express = require('express'),
 		bodyParser = require('body-parser'),
 		nodeMailer = require('nodemailer');
 
+require('dotenv').config();
 const app = express();
 const port = 3000;
 
@@ -12,26 +13,45 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+
 app.get('/', (req, res) => res.render('index'));
 
 app.post('/contact-me', (req, res) => {
+
+	const output = `
+		<h1>You have a new message from your portfolio!</h1>
+		<li>Name: ${req.body.name}</li>
+		<li>Name: ${req.body.email}</li>
+		<br>
+		<p>Message: ${req.body.message}</p>
+	`
+	
 	let transporter = nodeMailer.createTransport({
           host: 'smtp.gmail.com',
           port: 465,
           secure: true,
           auth: {
-	          user: 'email',
-	          pass: 'password'
+	          user: process.env.NM_USER,
+	          pass: process.env.NM_PASS
       	}
     });
 
     let mailOptions = {
-      from: 'from-email', // sender address
-      to: ['to-email'], // list of receivers
+      from: 'webdevpchoi@gmail.com', // sender address
+      to: 'pchoi13370@gmail.com', // list of receivers
       subject: req.body.subject, // Subject line
       text: req.body.message, // plain text body
-      html: '<b>This email actually worked!</b>' // html bodyParser
+      html: output // html bodyParser
 	};
+
+	transporter.sendMail(mailOptions, function (err, info) {
+	   if(err)
+	     console.log(err)
+	   else
+	     console.log('mail sent!');
+	});
+
+
 });
 
 app.listen(port, () => console.log('Server is running!'));
