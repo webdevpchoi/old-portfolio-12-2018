2e6af36be99754a21722904bf228712858131c7d
Phillip Choi <pchoi13370@gmail.com>
2018-05-29 23:24:29 -0700

    changed port number so heroku could use

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const 	express = require('express'),
 
 require('dotenv').config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
